259c724ab4185c5adb8684ad02ba969c34540c56
NellitG <kamaunelson157@gmail.com>
2025-02-19 19:29:03 +0300

    URL.py

--- venv/tennis/members/urls.py
+++ venv/tennis/members/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import include, path
+# from . import views
 
 urlpatterns = [
-    path('members/', views.members, name='members'),
+    path('members', include('members.urls')),
+    path('admin/', admin.site.urls),
+    # path('members/', include('members.urls')),
 ]
